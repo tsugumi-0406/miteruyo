29c925d1ee54505b019f107d19f69c26a335aca9
tsugumi <vampire5.9.jigsaw@gmail.com>
2026-07-16 15:21:28 +0900

    ログインページの修正

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -4,12 +4,14 @@ import Input from "../components/input";
 function App() {
     return (
       <div className="grid grid-cols-1 gap-4">
-        <h1 className="bold text-3xl py-10">ログイン</h1>
+        <h1 className="font-bold text-3xl py-10">ログイン</h1>
         <Input label="メールアドレス" />
-        <Input label="パスワード"/>
+        <Input label="パスワード" />
         <LoginButton inner="ログイン" />
         <div className="text-center my-10">
-          <a href="" className="underline">会員登録はこちら</a>
+          <a href="" className="underline">
+            会員登録はこちら
+          </a>
         </div>
       </div>
     );
