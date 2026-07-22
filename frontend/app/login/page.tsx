4a40631e665927f4f74aecc0d0b20a57d659e813
tsugumi <vampire5.9.jigsaw@gmail.com>
2026-07-22 13:10:51 +0900

    ホーム画面の作成

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,18 +1,21 @@
-import LoginButton from "../components/button";
+import { LoginButton, RecordButton } from "../components/button";
+
 import Input from "../components/input";
 
 function App() {
-    return (
-      <div className="grid grid-cols-1 gap-4">
-        <h1 className="bold text-3xl py-10">ログイン</h1>
-        <Input label="メールアドレス" />
-        <Input label="パスワード"/>
-        <LoginButton inner="ログイン" />
-        <div className="text-center my-10">
-          <a href="" className="underline">会員登録はこちら</a>
-        </div>
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      <h1 className="font-bold text-3xl py-10">ログイン</h1>
+      <Input label="メールアドレス" />
+      <Input label="パスワード" />
+      <LoginButton inner="ログイン" />
+      <div className="text-center my-10">
+        <a href="" className="underline">
+          会員登録はこちら
+        </a>
       </div>
-    );
+    </div>
+  );
 }
 
 export default App;
